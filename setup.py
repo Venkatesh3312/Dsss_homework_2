0686e4a72b3358d529f15affd4e8e16ba49d849c
Venkatesh3312 <150520616+Venkatesh3312@users.noreply.github.com>
2023-11-11 23:36:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Dsss_homework_2',
+    name='dsss_homework_2',
     version='3.11.6',
     packages=find_packages(),
-    entry_points=[
+    entry_points={
         'console_scripts': [
             'math_quiz = math_quiz.quiz:math_quiz_game',
         ],
-    ],
-    test_suite='tests',
+    },
 )
