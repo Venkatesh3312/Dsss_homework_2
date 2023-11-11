e05e5e78ef7030d60735a961bfe4330daaa62be1
Venkatesh3312 <150520616+Venkatesh3312@users.noreply.github.com>
2023-11-11 23:19:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,6 @@ setup(
     name='math_quiz',
     version='3.11.6'
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'math_quiz = math_quiz.quiz:math_quiz',
-        ],
-    },
-    install_requires=[
-        # Add any dependencies your project might have
-        'random',
+    entry_points=[
     ],
-    test_suite='tests',
 )
